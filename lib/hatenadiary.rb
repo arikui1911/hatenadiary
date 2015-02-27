82a32d67336a240ef0a40678f7fe0dc910b715fd
Arikui <arikui.ruby@gmail.com>
2015-02-28 04:22:49 +0900

    write YARD doc

--- lib/hatenadiary.rb
+++ lib/hatenadiary.rb
@@ -1,6 +1,17 @@
 require 'mechanize'
 
 class HatenaDiary
+  # With block, create a new client and login, in addition, logout at end of block ensurely.
+  #
+  # Without block, only create a new client and return it.
+  #
+  # @return [Object]      yield returned value
+  # @return [HatenaDiary] initialized HatenaDiary instance
+  #
+  # @param (see #initialize)
+  #
+  # @see #initialize HatenaDiary#initialize (has same parameters but block)
+  #
   def self.login(*args)
     client = new(*args)
     return unless block_given?
@@ -12,6 +23,21 @@ class HatenaDiary
     end
   end
 
+  # Create new HatenaDiary client.
+  #
+  # @return [HatenaDiary] initialized HatenaDiary instance
+  #
+  # @raise [ArgumentError] invalid user_agent_alias name
+  #
+  # @param [String]                 username         Hatena ID
+  # @param [String]                 password         Password for account of username
+  # @param [String]                 groupname        For "hatena group diary", See group page's URL; xxx.g.hatena.ne.jp
+  # @param [Number]                 read_timeout_sec Timeout seconds for agent.
+  # @param [String]                 user_agent_alias desc
+  # @param [Array<String, Integer>] http_proxy       Two-length array; ["host", port]
+  # @param [String]                 cookie_file_path Using cookie file
+  # @param [Encoding, String]       hatena_encoding  Posting texts' encoding
+  #
   def initialize(username, password,
                  groupname: nil, read_timeout_sec: nil, user_agent_alias: nil,
                  http_proxy: nil, cookie_file_path: nil,
@@ -31,14 +57,33 @@ class HatenaDiary
     @login_p = false
   end
 
+  # Make inspection (do not reveal password).
+  #
+  # @return [String] inspection
+  #
   def inspect
     "#<HatenaDiary:#{@username}>"
   end
 
+  # Returns already logined or not.
+  #
+  # Even if use cookie, return false before #login called.
+  #
+  # @return [Boolean] Already logined, or not
+  #
   def login?
     @login_p
   end
 
+  # Try to login (or load cookie).
+  #
+  # @return [void]
+  #
+  # @raise [Errno::ENOENT] When assign cookie file and no such file
+  # @raise [Mechanize::Error]
+  # @raise [Net::HTTP::Persistent::Error]
+  # @raise [TimeoutError]
+  #
   def login
     if @cookie_file
       @agent.cookie_jar.load @cookie_file
@@ -61,6 +106,15 @@ class HatenaDiary
     end
   end
 
+  # Try to logout (or save cookie).
+  #
+  # @return [void]
+  #
+  # @raise [Errno::ENOENT] When assign cookie file and no such file
+  # @raise [Mechanize::Error]
+  # @raise [Net::HTTP::Persistent::Error]
+  # @raise [TimeoutError]
+  #
   def logout
     if @cookie_file
       @agent.cookie_jar.save @cookie_file
@@ -70,6 +124,19 @@ class HatenaDiary
     @login_p = false
   end
 
+  # Post an entry to Hatena diary service.
+  #
+  # @return [void]
+  #
+  # @raise [HatenaDiary::LoginError] raise when login not yet
+  #
+  # @param [Integer] yyyy    Year
+  # @param [Integer] mm      Month
+  # @param [Integer] dd      Day
+  # @param [String]  title   Entry title
+  # @param [String]  body    Entry content text
+  # @param [Boolean] trivial "Trivial update" flag
+  #
   def post(yyyy, mm, dd, title, body, trivial: false)
     login_error "not logined yet" unless login?
     form = edit_form(yyyy, mm, dd){|r| r.form_with(name: 'edit') }
@@ -82,6 +149,16 @@ class HatenaDiary
     @agent.submit form, form.button_with(name: 'edit')
   end
 
+  # Delete an entry from Hatena diary service.
+  #
+  # @return [void]
+  #
+  # @raise [HatenaDiary::LoginError] raise when login not yet
+  #
+  # @param [Integer] yyyy Year
+  # @param [Integer] mm   Month
+  # @param [Integer] dd   Day
+  #
   def delete(yyyy, mm, dd)
     login_error "not logined yet" unless login?
     edit_form(yyyy, mm, dd){|r| r.forms.last }.submit
@@ -116,6 +193,7 @@ class HatenaDiary
       sprintf "http://%s.g.hatena.ne.jp/%s/edit?date=%04d%02d%02d", @groupname, @username, yyyy, mm, dd
     end
   end
+  private_constant :GroupDiary
 
   class LoginError < RuntimeError
     def initialize(msg, username, groupname = nil)
